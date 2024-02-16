be664c276142c7283b947637d6cff5a0fd76daf5
Andy Fleming <andy@ulendo.com>
2024-02-16 12:19:39 +0200

    Update

--- buildroom.js
+++ buildroom.js
@@ -1,22 +1,30 @@
 const Excel = require('exceljs')
 const fs = require('fs')
+const { DateTime } = require("luxon")
 
-
-//const catalogFile = 'D:\\Western Power Company\\Western Power Company\\WPC Working - Documents\\PRM Project Management\\Catalogue\\202402 WPC Documents Catalogue.xlsx'
-//const outFolder = 'D:\\temp\\CapLink\\'
-
-const catalogFile = 'D:\\OneDrive\\Western Power Company\\WPC Working - Documents\\PRM Project Management\\Catalogue\\202402 WPC Documents Catalogue.xlsx'
-const outFolder = 'D:\\OneDrive\\Western Power Company\\WPC Dataroom - Dataroom\\CapLink\\'
-
-
-const filterColumn = 'CAPLINK Dataroom'
-
-const sps = {
-  //WPCWorking: 'D:\\Western Power Company\\Western Power Company\\WPC Working - Documents',
-  //WPCHSESManagementSystem: 'D:\\Western Power Company\\Western Power Company\\WPC HSES Management System - General'
-  WPCWorking: 'D:\\OneDrive\\Western Power Company\\WPC Working - Documents',
-  WPCHSESManagementSystem: 'D:\\OneDrive\\Western Power Company\\WPC HSES Management System - General' 
+const configs = {
+  sable: {
+    catalogFile: 'D:\\Western Power Company\\Western Power Company\\WPC Working - Documents\\PRM Project Management\\Catalogue\\202402 WPC Documents Catalogue.xlsx',
+    outFolder: 'D:\\temp\\CapLink\\',
+    sps: {
+      WPCWorking: 'D:\\Western Power Company\\Western Power Company\\WPC Working - Documents',
+      WPCHSESManagementSystem: 'D:\\Western Power Company\\Western Power Company\\WPC HSES Management System - General'
+    }
+  },
+  cloud: {
+    catalogFile: 'D:\\Western Power Company\\Western Power Company\\WPC Working - Documents\\PRM Project Management\\Catalogue\\202402 WPC Documents Catalogue.xlsx',
+    outFolder: 'D:\\temp\\CapLink\\',
+    sps: {
+      WPCWorking: 'D:\\Western Power Company\\Western Power Company\\WPC Working - Documents',
+      WPCHSESManagementSystem: 'D:\\Western Power Company\\Western Power Company\\WPC HSES Management System - General'
+    }
+  }  
 }
+
+const config = configs.sable
+const catalogFile = config.catalogFile
+const outFolder = config.outFolder
+const filterColumn = 'CAPLINK Dataroom'
 
 const patt = /\.[^.\\/:*?"<>|\r\n]+$/i
 
@@ -36,12 +44,13 @@ async function main() {
       if (row.values[columnDict[filterColumn]]) { 
         console.log('Row ' + rowNumber + ' = ' + row.values[columnDict['Short Title']])
         
+        const fileDate = DateTime.fromISO(row.values[columnDict['DateString']])
         let outPath = outFolder + row.values[columnDict['Area']]
         const title = row.values[columnDict['Short Title']]
         const extension = row.values[columnDict['Name']].match(patt)[0]
 
-        const inLocation = sps[row.values[columnDict['SPSite']].result] + '\\' + decodeURI(row.values[columnDict['SPLocation']].result).replace('\General/','/').replace('%26','&').replace('/Shared Documents/', '')
-       // console.log(inLocation)
+        const inLocation = config.sps[row.values[columnDict['SPSite']].result] + '\\' + decodeURI(row.values[columnDict['SPLocation']].result).replace('\General/','/').replace('%26','&').replace('/Shared Documents/', '')
+
         try {
           if (!fs.existsSync(outPath)) {
             fs.mkdirSync(outPath)
@@ -58,7 +67,7 @@ async function main() {
               fs.mkdirSync(outPath)
             }
           }
-          const outLocation = outPath + '\\' + title + extension 
+          const outLocation = outPath + '\\' + fileDate.toFormat('yyyyLLdd') + ' ' + title.trim() + extension.trim()
           if (!fs.existsSync(outLocation)) {
             if (!fs.existsSync(inLocation)) {
               console.log('Source folder does not exist:', inLocation)
@@ -66,8 +75,6 @@ async function main() {
               fs.copyFileSync(inLocation, outLocation)
             }
           }
-
-          
         } catch (err) {
           console.error(err)
         }
